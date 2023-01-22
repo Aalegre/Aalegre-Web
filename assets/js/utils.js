1b76a161ee4728fc164c2107b455eefa271cdaa9
Alberto Alegre - Desktop <contacto@alberto-alegre.com>
2023-01-22 15:02:50 +0100

    Syntax start

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,7 +7,7 @@ function Translate() {
     var newUrl = url.replace(host, replaceAll(host, ".", "-") + ".translate.goog") + "?_x_tr_sl=en&_x_tr_tl=" + userLang.slice(0, 2);
     console.log(newUrl);
     if (userLang === "en")
-        return;
+        userLang = "es";
     window.location.href = newUrl;
 }
 
@@ -17,4 +17,8 @@ function escapeRegExp(string) {
 
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function scrollOffset(element, offset) {
+    window.scrollTo({ top: (element.getBoundingClientRect().top + window.pageYOffset + offset), behavior: 'smooth' });
 }
